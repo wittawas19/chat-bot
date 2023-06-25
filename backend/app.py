e984acb22df0d06ff240dcbc5a8ba7b3617e9b19
wittawas19 <wittawas19@gmail.com>
2023-06-25 21:29:53 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from bson import ObjectId, ISODate
 
 app = Flask(__name__)
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017')
 db = client['db_chatbot']
 chats_collection = db['chatRoom']
+message_collection = db['message']
 
 
 @app.route('/addChat', methods=['POST'])
@@ -15,7 +17,19 @@ def create_chat():
     chat_id = chats_collection.insert_one(data).inserted_id
     return jsonify({'message': 'Chat created successfully', 'chat_id': str(chat_id)}), 201
 
+@app.route('/sendMessage', methods = ['POST'])
+def send_message(chatId, order):
+    data = request.get_json()
 
+    message = {
+        "id" : ObjectId(),
+        "chat_id" : chatId, 
+        "order_num":  order,
+        "date_created" : ISODate(),
+    } 
+
+    id = message_collection.insert_one(data)
+    
 
 
 if __name__ == '__main__':
