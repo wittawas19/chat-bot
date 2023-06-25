0f1199a51c074c87c9f2a492d14ff5a280808088
wittawas19 <wittawas19@gmail.com>
2023-06-25 21:44:08 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -18,18 +18,29 @@ def create_chat():
     return jsonify({'message': 'Chat created successfully', 'chat_id': str(chat_id)}), 201
 
 @app.route('/sendMessage', methods = ['POST'])
-def send_message(chatId, order):
+def send_message(chatId):
     data = request.get_json()
 
     message = {
         "id" : ObjectId(),
-        "chat_id" : chatId, 
-        "order_num":  order,
+        "chat_id" : data.get("chat_id"), 
+        "order_num":  data.get("order_num"),
         "date_created" : ISODate(),
+        "content":  data.get("content"),
+        "has_file": data.get("has_file"), 
+        "file_path": data.get("file_path")
     } 
 
-    id = message_collection.insert_one(data)
-    
+    result = chats_collection.update_one(
+        {'_id': ObjectId(chatId)},
+        {'$push': {'messages': message}}
+    )
+
+    if result.modified_count > 0:
+        return jsonify({'message': 'Message added successfully'}), 201
+    else:
+        return jsonify({'message': 'Failed to add message'}), 400
+
 
 
 if __name__ == '__main__':
